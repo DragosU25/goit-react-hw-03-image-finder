90ad5be03bc0f70dd03c7091eb8762f3530600c5
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-07-14 20:41:34 +0200

    done

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import ImageGalleryItem from 'components/ImageGallleryItem';
+
+import styles from './ImageGallery.module.css';
+
+class ImageGallery extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { images, handleClick } = this.props;
+    return (
+      <ul className={styles.gallery}>
+        {images.map(image => (
+          <ImageGalleryItem
+            key={image.id}
+            image={image}
+            onClick={() => handleClick(image.largeImageURL)}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default ImageGallery;
